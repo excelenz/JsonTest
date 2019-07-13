53c7d7ede61dc4c8840e665cfc8bc4ced7cd0167
steppenwolf <margolin@visart.biz>
2019-07-14 01:18:12 +0300

    ff

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import logging
 
 class ParserJson:
     def __init__(self):
-        logging.basicConfig(filename='telegram.log',format="%(asctime)s:   %(message)s \n",datefmt='%Y-%m-%d,%H:%M',level=logging.INFO)
+        self.logging()
+        self.DEBUG=1
+
+    def logging(self):
+        logging.basicConfig(filename='telegram.log',format="%(asctime)s:  %(message)s \n",datefmt='%Y-%m-%d,%H:%M',level=logging.INFO)
         logger = logging.getLogger(__name__)
         logger.info("*************************************************************** \n ")
         logger.info(__name__)
@@ -12,19 +16,18 @@ class ParserJson:
     def main(self):
         return self.input_json()
 
-    def is_json(self):
-        try:
-            json_object = json.loads(json)
-        except Exception as e:
-            return False
-        return True
-
     def input_json(self):
         with open('data.txt') as json_file:
             try:
                 data = json.load(json_file)
+                logger = logging.getLogger("Success on open file")
+                return data
             except Exception as e:
+                logger = logging.getLogger("ERROR ON LOAD %s" % e)
+                if self.DEBUG:
+                    raise
                 return False
+        return True
 
 if __name__== '__main__':
     json_object=ParserJson()
