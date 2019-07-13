ceebfc715f434fc14b8b3855b6df0da15719e6da
steppenwolf <margolin@visart.biz>
2019-07-14 00:59:41 +0300

    11

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python
 import json
+import logging
 
 class ParserJson:
     def __init__(self):
-        1==1
-    def run(self):
-        1==1
-    def is_json(json):
+        logging.basicConfig(filename='telegram.log',format="%(asctime)s:   %(message)s \n",datefmt='%Y-%m-%d,%H:%M',level=logging.INFO)
+        logger = logging.getLogger(__name__)
+        logger.info("*************************************************************** \n ")
+        logger.info(__name__)
+
+    def main(self):
+        return self.input_json()
+
+    def is_json(self):
         try:
             json_object = json.loads(json)
         except Exception as e:
             return False
         return True
 
+    def input_json(self):
+        with open('data.txt') as json_file:
+            try:
+                data = json.load(json_file)
+            except Exception as e:
+                return False
+
 if __name__== '__main__':
     json_object=ParserJson()
-    json_object.run()
-    js={
-    "transmitter": "abc:123456",
-    "msg_time": "2019-03-15T10:26:37.951Z",
-    "msg_type": 83,
-    "message": "Hello World"}
+    json_object.main()
 
